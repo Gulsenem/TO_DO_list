0e7845320253e10d5763c81d2131e56ff2ce9ca6
Gulsenem <og.durdyeva@gmail.com>
2021-06-15 21:06:16 +0200

    html, css

---
+++ script.js
@@ -0,0 +1,18 @@
+var input = document.getElementById("input");
+
+
+function add()
+{
+    var div = document.createElement("div");
+    div.classList.add("liste");
+
+    var lisLeft = document.createElement("div");
+    lisLeft.innerHTML = input.value;
+
+    var lisRight = document.createElement("div");
+    lisRight.classList.add("liste_right");
+
+    var span = document.createElement("span");
+    
+
+}
