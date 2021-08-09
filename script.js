86215a3239f5cf1c1318198774953517f4c10f59
Gulsenem <og.durdyeva@gmail.com>
2021-08-09 22:31:47 +0200

    Edit, check, kreuz function

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var input = document.getElementById("input");
 var container = document.getElementById("container");
+let add_btn = document.getElementById("add");
 
 function add()
 {
-    
+    if(input.value == "")
+    {
+        alert("Bitte Ihre Ziel schreiben!");
+        return;
+    }
+
     var liste = document.createElement("div");
     liste.classList.add("liste");
 
@@ -15,17 +21,53 @@ function add()
 
     var span = document.createElement("span");
     span.innerHTML = "Edit";
+    span.onclick = function()
+    {
+        if(span.innerHTML =="Edit")
+        {
+        input.value = lisLeft.innerHTML;
+        lisLeft.innerHTML = "";
+        span.innerHTML = "ok";  
+        add_btn.disabled = true;      
+     
+        }
+         
+    
+        else if(span.innerHTML =="ok")
+        {
+            lisLeft.innerHTML = input.value;
+            span.innerHTML = "Edit";
+            input.value ="";
+            add_btn.disabled = false; 
+            check.disabled = true;
+            kreuz.disabled = true;
+
+        }   
+
+    }
 
     var check = document.createElement("button");
     check.classList.add("check");
-    check.onclick = checked;
+    check.onclick = function()
+    {
+        lisLeft.style.textDecoration = "line-through";
+        lisLeft.style.color = "gold";
+        this.innerHTML = "&#128512";
+        this.style.fontSize = "28px";
+        this.style.backgroundColor = "inherit";
+        this.disabled = true;
+        span.disabled = true;
+    };
     
     var check_icon = document.createElement("i");
     check_icon.classList.add("fas", "fa-check");
 
     var kreuz = document.createElement("button");
     kreuz.classList.add("kreuz");
-    kreuz.onclick = deleted;
+    kreuz.onclick = function()
+    {
+        liste.style.display = "none";
+    };
 
     var kreuz_icon = document.createElement("i");
     kreuz_icon.classList.add("fas", "fa-times");
@@ -41,15 +83,5 @@ function add()
 
     container.appendChild(liste);
 
-
-}
-
-function checked()
-{
- 
-}
-
-function deleted()
-{
-    
+    input.value ="";
 }
